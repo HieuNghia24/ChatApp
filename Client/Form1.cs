31619d4c40aed6a6dc037c06be710c6c9bab98c3
ASUS <ASUS@DESKTOP-RKVHRND>
2021-11-17 22:58:21 +0700

    Add project files.

---
+++ Client/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Client
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            Connect();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+
+            Send();
+            AddMessage(txbMessage.Text);
+
+        }
+
+        IPEndPoint IP;
+        Socket client;
+        // Kết nối tới server
+        void Connect()
+        {
+            //IP: Địa chỉ của server
+            IP = new IPEndPoint(IPAddress.Parse("127.0.0.11"), 9999);
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            try
+            {
+                client.Connect(IP);
+            }
+            catch {
+                MessageBox.Show("Khong the ket noi den SERVER!");
+                return;
+                    }
+
+            Thread listen = new Thread(Receive);
+            listen.IsBackground = true;
+            listen.Start();
+        }
+
+        // Đóng kết nối hiện thời
+        void Close()
+        {
+            client.Close();
+        }
+
+        // Gửi tin
+        void Send()
+        {
+            if (txbMessage.Text != string.Empty)
+                client.Send(Serialize(txbMessage.Text));
+        }
+
+        // Nhận tin
+        void Receive()
+        {
+            try
+            {
+                while (true)
+                {
+                    byte[] data = new byte[1024 * 5000];
+                    client.Receive(data);
+                    string message = (string)Deserialize(data);
+                    AddMessage(message);
+                }
+
+            }
+            catch {
+                Close(); } }
+       
+        // ADD MESSAGE vào khung chat
+        void AddMessage(string s)
+        {
+            lsvMessage.Items.Add(new ListViewItem() { Text = s }) ;
+            txbMessage.Clear();
+
+        }
+
+        // Phân mảnh
+        byte[] Serialize(object obj)
+        {
+            MemoryStream stream = new MemoryStream();
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, obj);
+            return stream.ToArray();
+
+        }
+
+        // Gom mảnh lại 
+        object Deserialize(byte[] data)
+        {
+            MemoryStream stream = new MemoryStream(data);
+            BinaryFormatter formatter = new BinaryFormatter();
+            return formatter.Deserialize(stream);
+            
+        }
+
+        // Đóng kết nối khi đóng form
+        private void Client_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Server/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            Connect();
+        }
+
+        // Đóng kết nối
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Close();
+        }
+
+        // Gửi tin cho tất cả client
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            foreach (Socket item in clientList)
+            {
+                Send(item); 
+            }
+            txbMessage.Clear();
+        }
+
+        IPEndPoint IP;
+        Socket server;
+        List<Socket> clientList;
+
+        // Kết nối tới server
+        void Connect()
+        {
+            // IP: Địa chỉ của server
+            clientList = new List<Socket>();
+            IP = new IPEndPoint(IPAddress.Any, 9999);
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+            server.Bind(IP);
+            Thread Listen = new Thread(() =>
+            {
+                try
+                {
+                    while (true)
+                    {
+                        server.Listen(100);
+                        Socket client = server.Accept();
+                        clientList.Add(client);
+
+                        Thread receive = new Thread(Receive);
+                        receive.IsBackground = true;
+                        receive.Start(client);
+                    }
+                }
+                catch
+                {
+                    IP = new IPEndPoint(IPAddress.Any, 9999);
+                    server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+                }
+
+            });
+            Listen.IsBackground = true;
+            Listen.Start();
+        }
+
+        // Đóng kết nối hiện thời
+        void Close() 
+        {
+            server.Close();
+        }
+
+        // Gửi tin
+        void Send(Socket client)
+        {
+            if (txbMessage.Text != string.Empty)
+                client.Send(Serialize(txbMessage.Text));
+        }
+
+        // Nhận tin
+        void Receive(object obj)
+        {
+            Socket client = obj as Socket; 
+            try
+            {
+                while (true)
+                {
+                    byte[] data = new byte[1024 * 5000];
+                    client.Receive(data);
+                    string message = (string)Deserialize(data);
+                    AddMessage(message);
+                }
+
+            }
+            catch
+            {
+                clientList.Remove(client);
+                client.Close();
+            }
+        }
+        
+        // ADD MESSAGE vào khung chat 
+        void AddMessage(string s)
+        {
+            lsvMessage.Items.Add(new ListViewItem() { Text = s });
+            txbMessage.Clear();
+
+        } 
+
+        // Phân mảnh
+        byte[] Serialize(object obj)
+        {
+            MemoryStream stream = new MemoryStream();
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, obj);
+            return stream.ToArray();
+
+        }
+
+        // Gom mảnh lại
+        object Deserialize(byte[] data)
+        {
+            MemoryStream stream = new MemoryStream(data);
+            BinaryFormatter formatter = new BinaryFormatter();
+            return formatter.Deserialize(stream);
+
+        }
+    }
+}
